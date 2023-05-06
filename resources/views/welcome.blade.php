6fee6d6bd65d9675b52d7afff35d2773f45bab5b
Mohammad Al-Nur Sarwer Arif <alnurarif@yahoo.com>
2023-05-06 23:16:43 +0600

    it changes the title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" id="token" content="{{ csrf_token() }}">
-        <title>Small Store</title>
+        <title>Small Todo</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
         {{-- @vite('resources/css/app.css') --}}
         <link rel="stylesheet" href="{{asset('css/app-922580d8.css')}}">
